43231a94fe17bc50a9ab85cb29e1efaeec8ae725
x0PuffBaddie <villamoralslenicole@gmail.com>
2025-05-22 13:11:17 +0700

    injected api

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-function generateEplaination(event) {
-    event.preventDefault();
-
-let jargonFormElement = document.querySelector("#explaination");
+function displayExplaination(response) {
 
 new Typewriter('#explaination', {
-  strings: ['explain to me like Im 5'],
+  strings: response.data.answer,
   autoStart: true,
   delay: 1,
   cursor:"",
+  loop: false,
 });
 
 }
 
+
+function generateExplaination(event) {
+    event.preventDefault();
+
+    
+let userInstructionInput =document.querySelector("#user-instruction");
+let apiKey ="0f47325f67190a100b7be47t71b9a1ob";
+let prompt =`Generate a web3 jargon meaning ${userInstructionInput.value}`;
+let context =`User-instruction: You are a web3 expert and your task is to explain cyrpto or web3 jargons to your student like they're 15 years old. Provide a source link as an example project to understand it more clearly`;
+let apiUrl =`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+
+
+axios.get(apiUrl).then(displayExplaination);
+
+}
+
+
 let jargonFormElement = document.querySelector("#jargon-generator-form");
-jargonFormElement.addEventListener("submit", generateEplaination);
+jargonFormElement.addEventListener("submit", generateExplaination);
